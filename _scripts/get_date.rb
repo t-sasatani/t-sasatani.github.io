5b8f45375e6d33c48af973b0fcb1a3183a976eba
t-sasatani <33111879+t-sasatani@users.noreply.github.com>
2024-08-14 14:45:29 +0900

    another minor typo fix

--- _scripts/get_date.rb
+++ _scripts/get_date.rb
@@ -4,7 +4,7 @@ require 'date'
 current_date = Date.today.to_s
 yaml_file_path = '_data/metadata.yml'
 data = YAML.load_file(yaml_file_path)
-metadata['gheet_update'] = current_date
+metadata['gsheet_update'] = current_date
 File.open(yaml_file_path, 'w') do |file|
   file.write(data.to_yaml)
 end
