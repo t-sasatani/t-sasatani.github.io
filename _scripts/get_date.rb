42295aa575eadbd7b6a99ed1fe256540c5352a89
t-sasatani <33111879+t-sasatani@users.noreply.github.com>
2024-08-14 14:49:46 +0900

    bug fix date update script

--- _scripts/get_date.rb
+++ _scripts/get_date.rb
@@ -3,7 +3,7 @@ require 'date'
 
 current_date = Date.today.to_s
 yaml_file_path = '_data/metadata.yml'
-data = YAML.load_file(yaml_file_path)
+metadata = YAML.load_file(yaml_file_path)
 metadata['gsheet_update'] = current_date
 File.open(yaml_file_path, 'w') do |file|
   file.write(data.to_yaml)
