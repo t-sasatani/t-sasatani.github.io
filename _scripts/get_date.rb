de79bef35a80c705f29a7685d1deea4754ed16bb
t-sasatani <33111879+t-sasatani@users.noreply.github.com>
2024-08-14 14:36:53 +0900

    add update date

---
+++ _scripts/get_date.rb
@@ -0,0 +1,12 @@
+require 'yaml'
+require 'date'
+
+current_date = Date.today.to_s
+yaml_file_path = '_data/metadata.yml'
+data = YAML.load_file(yaml_file_path)
+metadata['gheet_update'] = current_date
+File.open(yaml_file_path, 'w') do |file|
+  file.write(data.to_yaml)
+end
+
+puts "Date updated to #{current_date} in #{yaml_file_path}"
